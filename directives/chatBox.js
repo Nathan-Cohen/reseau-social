fdcdd8c3f425b336c486205396b8805e2bd0dbb0
Nathan <manathane.co@gmail.com>
2018-10-30 15:51:40 +0100

    chatbox

--- directives/chatBox.js
+++ directives/chatBox.js
@@ -5,13 +5,18 @@ m.directive('chatbox', function(){
                 $scope.testAfficheChatBot = true
             }
 
+            $scope.envoieMessage = function(){
+                console.log('testenvoiemsg', $scope.msgBox)
+                $scope.msgBox = ''
+            }
+
             $scope.testfunc = function(){
                 SocketService.emit('test', {message: 'ledirectcesttropchaud'});
                 console.log('testfront', SocketService)
             }
         },
         template: `
-            <div class="col-md-5 chatbox">
+            <div class="col-md-5 chatbox" ng-if="testAfficheChatBot">
                 <div class="panel panel-primary">
 
                     <div class="panel-collapse collapse" id="collapseOne">
@@ -45,42 +50,15 @@ m.directive('chatbox', function(){
                                         </p>
                                     </div>
                                 </li>
-                                <li class="left clearfix"><span class="chat-img pull-left">
-                                        <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
-                                                <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
-                                            dolor, quis ullamcorper ligula sodales.
-                                        </p>
-                                    </div>
-                                </li>
-                                <li class="right clearfix"><span class="chat-img pull-right">
-                                        <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
-                                            <strong class="pull-right primary-font">Bhaumik Patel</strong>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
-                                            dolor, quis ullamcorper ligula sodales.
-                                        </p>
-                                    </div>
-                                </li>
                             </ul>
                         </div>
                         <div class="panel-footer">
                             <div class="input-group">
-                                <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                                <input id="msgBox" ng-model="msgBox" type="text" class="form-control input-sm" placeholder="Type your message here..." />
                                 <span class="input-group-btn">
-                                    <button class="btn btn-warning btn-sm" id="btn-chat">
-                                        Send</button>
+                                    <button ng-click="envoieMessage()" class="btn btn-warning btn-sm" id="btn-chat">
+                                        Send
+                                    </button>
                                 </span>
                             </div>
                         </div>
